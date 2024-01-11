4cfcd81b4d767d9556ced11ffa5ef49651a393aa
Dinuwan <Kavindudb123@gmail.com>
2024-01-11 10:23:32 +0530

    initial update

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -19,19 +19,23 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
         val cost = binding.costOfService.text.toString().toDouble()
         val selectedId = binding.tipOptions.checkedRadioButtonId
+        val userPercentage = binding.otherPercentage.text.toString().toDouble()
         val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_fifteen_percent -> 0.15
-            else -> 0.10
+            R.id.option_ten_percent -> 0.10
+            else -> userPercentage/100
         }
         var tip = tipPercentage * cost
         val roundUp = binding.roundTip.isChecked
         if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
+        var totalBill = cost + tip
         val locale = Locale("si", "LK")
         val formattedTip = NumberFormat.getCurrencyInstance(locale).format(tip)
-
+        val formattedTotalBill = NumberFormat.getCurrencyInstance(locale).format(totalBill)
+        binding.totalBill.text = getString(R.string.total_amount, formattedTotalBill)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
 
